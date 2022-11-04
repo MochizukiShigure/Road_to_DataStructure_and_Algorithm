81153dc53a32e06640b57b82b70a7b4f5095f45e
Mochizuki Shigure <84255253+MochizukiShigure@users.noreply.github.com>
2022-11-05 01:25:12 +0800

    Update assignment.c

--- Search/Implementation in C/assignment.c
+++ Search/Implementation in C/assignment.c
@@ -118,3 +118,104 @@ int main()
  printf("%d",c);
 }
 
+//general
+#include <stdio.h>
+int rarr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int i,j,k;
+//1.block
+struct index
+{
+	int key;  
+	int start;
+	int end;    
+}index_list[3];
+
+int creat_list(int a[])
+{
+	i=0;
+	j=0;
+	while(i<=2){
+		index_list[i].start=j;
+		j=j+2;
+		index_list[i].end=j;
+		for(k=index_list[i].start;k<=j;k++){
+			if(index_list[i].key < a[k]){
+				index_list[i].key = a[k];
+			}
+		}
+		j++;
+		i++;
+	}
+}
+
+int block_search(int key,int a[])  
+{
+	
+	for(i=0;i<=2&&key>index_list[i].key;i++)
+	{
+		
+	}
+	
+	for(j=index_list[i].start; j<=index_list[i].end; j++){
+		if(a[j]==key){
+			return j;
+			} 
+		}
+	
+	
+}
+
+//sequence
+int lenr = sizeof(rarr) / sizeof(rarr[0]);
+int sequence_search(int *a, int len, int k)
+{
+
+	for(i = 0 ;i <= len;i++){
+		if(a[i] == k)
+		{
+			return i;	
+		}
+	}
+	return -1;
+}
+
+//binary
+int binary_search(int key) 
+{
+ int left = 1,right = 9;
+ int mid = 0;
+ while (left <= right)
+ {
+  mid = (left + right) / 2;
+  if (key < mid)
+  {
+   right = mid;
+  }
+  else if (key > mid)
+  {
+   left = mid+1;
+  }
+  else 
+  {
+   return key;
+   break;
+  }	
+ }
+}
+
+int main(){
+	int key,a;
+	scanf("%d",&key);
+	creat_list(rarr);
+	a=block_search(key,rarr);
+	printf("result by block search : %d is founded at index %d in array \n", key, a);
+	
+	int b;
+	b = sequence_search(rarr, lenr, key);
+	printf("result by sequence search : %d is founded at index %d in array \n", key, b);
+	
+ 	int c = binary_search(key);
+ 	printf("result by binary search : %d is founded \n", c);
+ 
+	return 0;
+}
