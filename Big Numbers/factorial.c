d1356d65ccc76c1c8bd9dff5e6625f1d80cd967b
Mochizuki Shigure <84255253+MochizukiShigure@users.noreply.github.com>
2022-11-07 16:27:35 +0800

    Create factorial.c

---
+++ Big Numbers/factorial.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+#define MAX 100000
+
+int main(){
+
+int i,j,k,digit,temp;
+
+scanf("%d",&k);
+
+char arr[MAX+1] = {1};
+
+for(i=2; i<=k; i++){
+
+for(digit=0, j=0; j<=MAX; j++){
+
+temp = arr[j] * i + digit;
+
+digit = temp / 10;
+
+arr[j] = temp % 10;
+
+}
+
+}
+
+for(i = MAX;i>=0;i--){
+
+if(arr[i] != 0){
+
+for(i;i>=0;i--){
+
+printf("%d",arr[i]);
+
+}
+
+}
+
+}
+
+return 0;
+
+}
