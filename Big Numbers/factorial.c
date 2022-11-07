ad4cbc633f94662aee2eb39b09cc6d6e68a393a5
Mochizuki Shigure <84255253+MochizukiShigure@users.noreply.github.com>
2022-11-07 16:28:31 +0800

    Update factorial.c

--- Big Numbers/factorial.c
+++ Big Numbers/factorial.c
@@ -2,42 +2,43 @@
 
 #define MAX 100000
 
-int main(){
+int main()
+{
 
-int i,j,k,digit,temp;
+  int i,j,k,digit,temp;
 
-scanf("%d",&k);
+  scanf("%d",&k);
 
-char arr[MAX+1] = {1};
+  char arr[MAX+1] = {1};
 
-for(i=2; i<=k; i++){
+  for(i=2; i<=k; i++){
 
-for(digit=0, j=0; j<=MAX; j++){
+    for(digit=0, j=0; j<=MAX; j++){
 
-temp = arr[j] * i + digit;
+        temp = arr[j] * i + digit;
 
-digit = temp / 10;
+        digit = temp / 10;
 
-arr[j] = temp % 10;
+        arr[j] = temp % 10;
 
-}
+    }
 
-}
+  }
 
-for(i = MAX;i>=0;i--){
+  for(i = MAX;i>=0;i--){
 
-if(arr[i] != 0){
+    if(arr[i] != 0){
 
-for(i;i>=0;i--){
+      for(i;i>=0;i--){
 
-printf("%d",arr[i]);
+        printf("%d",arr[i]);
 
-}
+      }
 
-}
+    }
 
-}
+  }
 
-return 0;
+  return 0;
 
 }
