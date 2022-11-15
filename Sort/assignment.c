5fd5113d750dae2c78124b4c2adf75b1aab4873a
Mochizuki Shigure <84255253+MochizukiShigure@users.noreply.github.com>
2022-11-15 12:38:28 +0800

    Update assignment.c

--- Sort/assignment.c
+++ Sort/assignment.c
@@ -3,7 +3,29 @@
 */
 
 //insertion sort
+#include<stdio.h>
 
+void insertsort(int arr[], int len){
+	int i,j,k;
+	for (i=1;i<len;i++){
+		k = arr[i];
+		j=i-1;
+		while((j>=0) && (arr[j]>k)) {
+			arr[j+1] = arr[j];
+			j--;
+		}
+		arr[j+1] = k;
+	}
+}
+
+int main()
+{
+	int a[6] = {7,4,-2,19,13,6};
+	insertsort(a,6);
+	for(int i=0;i<=5;i++){
+		printf("%d ", a[i]);
+	}
+}
 
 //binary insertion sort
 
