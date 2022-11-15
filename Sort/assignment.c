182ffc352aa9fde19a2d781ba84b5d5d99be8b3e
Mochizuki Shigure <84255253+MochizukiShigure@users.noreply.github.com>
2022-11-15 12:56:05 +0800

    Update assignment.c

--- Sort/assignment.c
+++ Sort/assignment.c
@@ -28,6 +28,49 @@ int main()
 }
 
 //binary insertion sort
+#include <stdio.h>
 
+void binaryInsertSort(int arr[], int len)
+{
+	int i, j, k, low, high, mid;
+	for (i = 1; i < len; i++)
+	{
+		if (arr[i - 1] > arr[i])
+		{
+			k = arr[i];
+			low = 0;
+			high = i - 1;
+
+			while (low <= high)
+			{
+				mid = (low + high) / 2;
+				if (k <= arr[mid])
+				{
+					high = mid - 1;
+				}
+				else
+				{
+					low = mid + 1;
+				}
+			}
+			
+			for ( j = i - 1; j >= high + 1; j--)
+			{
+				arr[j + 1] = arr[j];
+			}
+			arr[high + 1] = k;
+		}
+	}
+}
+
+int main()
+{
+	int i, a[8] ={30,13,70,85,39,42,6,20} ;
+	binaryInsertSort(a, 8);
+	for (i = 0; i < 8; i++)
+	{
+		printf("%d ", a[i]);
+	}
+}
 
 //bubble sort
