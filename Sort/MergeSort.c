e75463c3283ceb9a71c218891624a9c48d7e722c
Mochizuki Shigure <84255253+MochizukiShigure@users.noreply.github.com>
2022-11-22 01:25:21 +0800

    Update MergeSort.c

--- Sort/MergeSort.c
+++ Sort/MergeSort.c
@@ -1,1 +1,53 @@
 
+//2 quick sort
+#include <stdio.h>
+void quickSort(int a[], int lower, int upper)
+{
+	int i,j,k,tmp,temp;
+	i = lower;
+	j = upper;
+	tmp = a[lower];   
+	
+	if(i > j)  
+	{
+		return;
+	}
+	while(i != j)
+	{
+		while(i < j && a[j] >= tmp)   
+		{
+			j--;
+		}
+		
+		while(i < j && a[i] <= tmp)
+		{
+			i++;
+		}
+		
+		if(j > i)
+		{
+			temp = a[j];
+			a[j] = a[i];
+			a[i] = temp;
+		}
+	}
+	
+	a[lower] = a[i];
+	a[i] = tmp;
+	quickSort(a,lower,i-1);
+	quickSort(a,i+1,upper);
+}
+
+int main()
+{
+	int i;
+	int a[]={3,11,5,4,64,43,6,9,8,99};
+	
+	quickSort(a, 0, 9);
+	for(i = 0; i < 10; i++)
+	{ 
+		printf(" %d ", a[i]);
+	}
+	
+	return 0;
+}
